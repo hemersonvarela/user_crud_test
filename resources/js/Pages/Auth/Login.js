92ca161e6092dea279319231554937112216002a
Hemerson Varela <hemerson.varela@gmail.com>
2022-03-04 17:55:36 -0500

    set credential for default user

--- resources/js/Pages/Auth/Login.js
+++ resources/js/Pages/Auth/Login.js
@@ -9,8 +9,8 @@ import { Head, Link, useForm } from '@inertiajs/inertia-react';
 
 export default function Login({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
+        email: 'admin@demo.com',
+        password: 'admin',
         remember: '',
     });
 
