57b2d62f105fd14b908ec61f584018537599cbab
Hemerson Varela <hemerson.varela@gmail.com>
2022-03-05 02:00:11 -0500

    create api resources

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'full_name' => $this->full_name,
+            'email' => $this->email,
+            'created_at' => $this->created_at->format('m/d/Y'),
+            'role' => $this->whenLoaded('role')
+        ];
+    }
+}
